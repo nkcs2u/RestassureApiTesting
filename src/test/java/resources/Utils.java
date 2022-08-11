9ee2de3efc06469e05f155be4e08a6563aa14e41
naveen <nkcs2u@gmail.com>
2022-08-11 17:01:13 +0530

    Changed the directory address for Global.properties file.

--- src/test/java/resources/Utils.java
+++ src/test/java/resources/Utils.java
@@ -40,7 +40,7 @@ public class Utils {
 	public static String getGlobalValue(String key) throws IOException
 	{
 		Properties prop =new Properties();
-		FileInputStream fis =new FileInputStream("C:\\Users\\rahul\\restassured\\APIFramework\\src\\test\\java\\resources\\global.properties");
+		FileInputStream fis =new FileInputStream(".\\src\\test\\java\\resources\\global.properties");
 		prop.load(fis);
 		return prop.getProperty(key);
 	
@@ -55,4 +55,6 @@ public class Utils {
 		JsonPath   js = new JsonPath(resp);
 		return js.get(key).toString();
 	}
+
+
 }
